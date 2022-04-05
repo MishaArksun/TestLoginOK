acf89d8ec8aef29e582c90561c8443eea31b17fe
MishaArksun <102590416+MishaArksun@users.noreply.github.com>
2022-04-05 17:17:50 +0300

    Update Test1.java

--- src/test/java/ru/ok/Test1.java
+++ src/test/java/ru/ok/Test1.java
@@ -2,14 +2,16 @@ package ru.ok;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+import java.util.ArrayList;
 
 public class Test1 {
     private ChromeDriver driver;
     private LoginPage loginPage;
     private MainPage mainPage;
-    private User user = new User("aaaa", "bbbb", "Миша Мельников");
-
+    private User user = new User("aaa", "aaa", "aaa");
     @Test
     public void Test(){
         System.setProperty("webdriver.chrome.driver", "C:\\tehnopolis\\chromedriver_win32\\chromedriver.exe");
@@ -20,5 +22,21 @@ public class Test1 {
         LoginPage loginPage = new LoginPage(driver);
         MainPage mainPage = loginPage.login(user.login, user.pass);
         Assert.assertEquals(mainPage.getName(), user.name);
+
+        driver.findElement(By.xpath("//*[@id='music_toolbar_button']/div[2]")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.xpath("//*[@id='music_layer']/header/div/div")).click();
+
+        ArrayList<MusicElement> musicElementList = new ArrayList<MusicElement>();
+
+        for(int i=1; i<5; i++){
+            musicElementList.add(new MusicElement(driver.findElement(By.xpath("//wm-tracks-list/main/wm-track[" + i + "]"))));
+        }
+
+        for(MusicElement a : musicElementList ){
+            System.out.println( a.getName());
+            a.add();
+        }
     }
 }
+
